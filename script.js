3c1ed8a2b0a6650a6f14c915eb7e3d6c9a26e240
Zieba <biankamaria.zieba@sunrise.net>
2023-10-18 16:12:52 +0200

    [INITIAL] Added Default Contact Form

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const labels = document.querySelectorAll('label');
+
+labels.forEach(label => {
+    label.addEventListener('click', () => {
+        labels.forEach(l => l.classList.remove('border-2', 'border-red-600'));
+        label.classList.add('border-2', 'border-red-600');
+    });
+});
